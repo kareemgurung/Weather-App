d3eb78d5d336ed4e00fcd66f8b3485d23ae6a500
Kareem Gurung <kareemgurung@Kareems-MacBook-Pro.local>
2020-10-20 14:42:07 +0100

    Completed solution

--- app.js
+++ app.js
@@ -1,10 +1,36 @@
+require("dotenv").config();
 const express = require("express");
+const https = require("https");
+
 const app = express();
 
 app.use(express.urlencoded({extended: true}));
 
 app.get("/", function(req, res){
     res.sendFile(__dirname + "/index.html");
+    
+});
+
+app.post("/", function(req, res){
+    const query = req.body.cityName;
+    const apiKey = process.env.API_KEY;
+    const unit = "metric";
+
+    const url = "https://api.openweathermap.org/data/2.5/weather?q=" + query + "&appid=" + apiKey + "&units=" + unit;
+    https.get(url, (response) => {
+        console.log(response.statusCode);
+        response.on("data", function (data) {
+            const weatherData = JSON.parse(data);
+            const temp = weatherData.main.temp;
+            const weatherDescription = weatherData.weather[0].description;
+            const icon = weatherData.weather[0].icon;
+            const imageURL = "https://openweathermap.org/img/wn/" + icon + "@2x.png"
+            res.write("<h1>The temperature in "+ query+" is " + temp + " degree Celcius. </h1>");
+            res.write("<p>The weather is currently " + weatherDescription + ".</p>");
+            res.write("<img src=" + imageURL + ">");
+            res.send();
+        });
+    });
 });
 
 app.listen(3000, function(){
